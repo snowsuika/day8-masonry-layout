5b4d62509670e892a8cf3480aee4b0a4bd3d909b
Snow <snowsuika@gmail.com>
2022-03-12 23:41:05 +0800

    init day8-masonry-layouts

---
+++ js/all.js
@@ -0,0 +1,34 @@
+const grid = document.querySelector('.grid');
+
+function renderImages() {
+    let items = '';
+    for (let i = 1; i <= 10; i++) {
+        items += `<div class="item">
+    <img src="../img/${i}.jpeg"/></div>`;
+    }
+    grid.innerHTML = items;
+    return items;
+}
+
+renderImages();
+
+var msnry = new Masonry(grid, {
+    itemSelector: '.item',
+    columnWidth: 20,
+    horizontalOrder: true,
+    gutter: 10,
+    percentPosition: true,
+});
+
+let time = 0;
+imagesLoaded(grid).on('progress', function (ImagesLoaded, LoadingImage) {
+    LoadingImage.img.style.animationName = 'bounceIn';
+    LoadingImage.img.style.animationDuration = '450ms';
+    LoadingImage.img.style.animationTimingFunction = 'linear';
+    LoadingImage.img.style.animationFillMode = 'forwards';
+    LoadingImage.img.style.animationDelay = time + 's';
+
+    // layout Masonry after each image loads
+    msnry.layout();
+    time += 0.15;
+});
